d3b3351ee798390a3d2231851d783f3a49f45351
alekLen <aleklenoch@gmail.com>
2024-05-08 11:09:24 +0300

    Initial commit

---
+++ src/test/java/com/example/javaforazure/JavaForAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaforazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaForAzureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
